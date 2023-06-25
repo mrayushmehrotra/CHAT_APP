438683a234c9e3c8367bbbc7355d84309bb3d7b0
Ayushoopp <ayushmehrotra099@>
2023-06-26 01:10:44 +0530

    Create A Login UI

---
+++ ichat/src/pages/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styles from "../styles/Join.module.css";
+import Image from 'next/image';
+import Logo from "./joinlogo.png";
+import Link from 'next/link';
+
+let user;
+
+const sendUser =()=>{
+  user = document.getElementById("joinInput").value
+  document.getElementById("joinInput").value=""
+}
+const join = () => {
+  const [name, setname] = useState("")
+
+  return (
+    <div className={styles.JoinPage}>
+      <div className={styles.JoinContainer}>
+        <Image
+          src={Logo}
+          width={100}
+          height={100}
+        />
+        <h1 className={styles.title}>ICHAT</h1>
+        <input onChange={(e)=>setname(e.target.value)} placeholder="Enter Your Name" type="text" id="joinInput" className={styles.joinInput} />
+        <Link 
+        onClick={(event)=> !name ?event.preventDefault():null} 
+        href="/Chat">
+          <button onClick={sendUser} className={styles.joinBtn}>Join</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default join;
+export {user}
